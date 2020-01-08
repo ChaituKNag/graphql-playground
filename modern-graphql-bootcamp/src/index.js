88441b3035c642ba168476510b4048c674a17432
nkonada <knc@outlook.in>
2020-01-08 16:41:17 +0530

    initial commit

---
+++ modern-graphql-bootcamp/src/index.js
@@ -0,0 +1,5 @@
+import { message } from "./my-module";
+import add, { subtract } from "./math";
+console.log(add(10, 20));
+console.log(subtract(30, 5));
+console.log(message);
---
+++ modern-graphql-bootcamp/src/my-module.js
@@ -0,0 +1,3 @@
+const message = "Hello, this is Chaitanya";
+
+export { message };
