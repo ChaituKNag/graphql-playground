053e5ec5d4d528e8a0cd8716122817a7a21ca5f5
nkonada <knc@outlook.in>
2020-07-05 22:12:01 +0530

    initial commit

--- modern-graphql-bootcamp/src/index.js
+++ modern-graphql-bootcamp/src/index.js
@@ -1,5 +1,71 @@
-import { message } from "./my-module";
-import add, { subtract } from "./math";
-console.log(add(10, 20));
-console.log(subtract(30, 5));
-console.log(message);
+import { GraphQLServer } from "graphql-yoga";
+
+// Scalar types - String, Boolean, Int, Float, ID
+
+// Type definitions
+const typeDefs = `
+    type Query {
+      me: User!
+      post: Post!
+      greeting(name: String): String!
+      add(a: Float!, b: Float!): Float!
+    }
+
+    type User {
+      id: ID!
+      name: String!
+      email: String!
+      age: Int
+    }
+
+    type Post {
+      id: ID!
+      title: String!
+      body: String!
+      published: Boolean!
+    }
+`;
+
+// Resolvers
+const resolvers = {
+  Query: {
+    greeting(parent, args, ctx, info) {
+      console.log(args);
+      if (args.name) {
+        return `Hello ${args.name}!`;
+      } else {
+        return `Hello!`;
+      }
+    },
+    me() {
+      return {
+        id: "123931",
+        name: "Naga",
+        email: "naga@example.com",
+        age: 31
+      };
+    },
+    post() {
+      return {
+        id: "abc123",
+        title: "This is awesome",
+        body: "Lorem ipsum",
+        published: true
+      };
+    },
+
+    add(_, args) {
+      if (args.a && args.b) {
+        return a + b;
+      }
+    }
+  }
+};
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => {
+  console.log("The server is up");
+});
--- modern-graphql-bootcamp/src/my-module.js
+++
@@ -1,3 +0,0 @@
-const message = "Hello, this is Chaitanya";
-
-export { message };
